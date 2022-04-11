41c1fad466044a78f8d1c4c749458896b807026d
Joakim <wahlstrom.joakim@gmail.com>
2022-04-11 10:42:42 +0200

    lektion 3

--- Lektion-3/1-shopping-list/src/App.js
+++ Lektion-3/1-shopping-list/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import { v4 as uuidv4} from 'uuid'
 import Header from './components/Header';
 import ShoppingList from './components/ShoppingList';
+import AddItemForm from './components/AddItemForm';
+import Modal from './components/Modal';
 
 function App() {
-
+  
+  const [showModal, setShowModal] = useState(false)
   const [items, setItems] = useState([
     {
       id: uuidv4(),
@@ -24,10 +27,24 @@ function App() {
     }
   ])
 
+
+  const clearList = () => {
+    setItems([])
+    setShowModal(false)
+  }
+
+  const addItem = (product) => {
+    setItems(prevItems => {
+      return [{ id: uuidv4(), product, completed: false }, ...prevItems]
+    })
+  }
+
   return (
     <div className="App container">
-      <Header title="Shopping List" />
+      <Header title="Shopping List" setShowModal={setShowModal} />
       <ShoppingList items={items} />
+      <AddItemForm addItem={addItem} />
+      { showModal && <Modal clearList={clearList} setShowModal={setShowModal} /> }
     </div>
   );
 }
