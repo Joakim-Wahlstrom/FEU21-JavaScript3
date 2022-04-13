481c0f97f02679ea0d0d4b1ad78d9b971b3abf68
Joakim <wahlstrom.joakim@gmail.com>
2022-04-13 10:35:10 +0200

    lektion 4

--- Lektion-4/1-use-effect/src/components/TodoList.jsx
+++ Lektion-4/1-use-effect/src/components/TodoList.jsx
@@ -1,26 +1,39 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import TodoItem from './TodoItem'
 import axios from 'axios'
+
+// const getTodos = async (url) => {
+//   const res = await axios.get(url)
+//   console.log('data')
+//   return res.data
+// }
 
 const TodoList = () => {
 
   const [todos, setTodos] = useState([])
   const [url, setUrl] = useState('http://localhost:8080/todos')
 
-  // axios.get('http://localhost:8080/todos')
-  //     // .then(res => console.log(res.data))
+  const getTodos = useCallback(async () => {
+    const res = await axios.get(url)
+    console.log('data')
+    setTodos(res.data)
+  }, [url])
+
+
+  // useEffect(() => {
+  //   axios.get(url)
   //     .then(res => {
-  //       console.log('data: ', res.data)
+  //       console.log('data')
   //       setTodos(res.data)
   //     })
+  // }, [url])
 
   useEffect(() => {
-    axios.get(url)
-      .then(res => {
-        console.log('data')
-        setTodos(res.data)
-      })
-  }, [])
+    // getTodos(url).then(setTodos)
+
+    getTodos()
+
+  },[getTodos])
 
   return (
     <div className='container todo-list'>
@@ -31,8 +44,8 @@ const TodoList = () => {
       }
 
 
-      <button className='btn mr'>All users</button>
-      <button className='btn'>user 1</button>
+      <button className='btn mr' onClick={() => setUrl('http://localhost:8080/todos')}>All users</button>
+      <button className='btn' onClick={() => setUrl('http://localhost:8080/todos?user=1')}>user 1</button>
     </div>
   )
 }
