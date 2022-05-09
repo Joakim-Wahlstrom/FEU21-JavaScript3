4ac79db4437e34527969d794539b3aaf0708e007
Joakim Wahlström <joakim@MacBook-Pro-som-tillhor-Joakim.local>
2022-05-09 10:43:28 +0200

    lektion 9

---
+++ Lektion-9/1-e-commerce/src/store/actiontypes.js
@@ -0,0 +1,11 @@
+const actiontypes = () => {
+    return {
+        productCatalog: {
+            getProductCatalog: 'GET_PRODUCT_CATALOG',
+            getProductCatalogSuccess: 'GET_PRODUCT_CATALOG_SUCCESS',
+            getProductCatalogFailure: 'GET_PRODUCT_CATALOG_FAILURE'
+        }
+    }
+}
+
+export default actiontypes
--- Lektion-9/1-e-commerce/src/views/Products.jsx
+++ Lektion-9/1-e-commerce/src/views/Products.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProductCatalog } from '../store/actions/productCatalogActions'
+
 
 const Products = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getProductCatalog())
+    }, [dispatch])
+
   return (
     <div>Products</div>
   )
